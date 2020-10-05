e1a0e6a4ea5c99b27ff5aa6233c3a86de5c4260b
marcus-py <71933698+marcus-py@users.noreply.github.com>
2020-10-05 14:05:02 -0400

    Create noun.py

---
+++ noun.py
@@ -0,0 +1,2 @@
+nouns = ['millimeters', 'discrepancies', 'lines', 'regulations', 'dent', 'cramp', 'term', 'ventilations', 'bracing', 'reserve', 'condenser', 'reservoirs', 'april', 'behaviors', 'savings', 'endings', 'dimensions', 'village', 'procurements', 'conspiracies', 'alert'
+'guards']
