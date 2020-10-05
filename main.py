8c6777f8c2f0a8b66bba62a82a1279a85fd4e6e3
marcus-py <71933698+marcus-py@users.noreply.github.com>
2020-10-05 15:05:58 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import random
 from adjectives import adjectives
 from nouns import nouns
-def username_gen(words):
+def username_gen():
   describerandom = random.randint(1,len(adjectives))
   thingrandom = random.randint(1, len(nouns))
   adjective_index = adjectives[describerandom]
   noun_index = nouns[thingrandom]
   randint = random.randint(1,1000)
-  password = str(adjective_index) + str(noun_index) + str(randint) 
-  print(password)
+  username = str(adjective_index) + '_' + str(noun_index) + str(randint) 
+  print(username)
 
-username_gen(['apple', 'banana', 'cranberry', 'doorknob', 'elephant'])
+username_gen()
